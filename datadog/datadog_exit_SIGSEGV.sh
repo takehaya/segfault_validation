cc805da52e719b025d63c0e7f165dd9fdf32eeb6
TakeruHayasaka <t-hayasaka@sakura.ad.jp>
2019-08-21 11:18:19 +0900

    fix shellscript

--- datadog/datadog_exit_SIGSEGV.sh
+++ datadog/datadog_exit_SIGSEGV.sh
@@ -7,4 +7,5 @@ k8s_state_data=$(kube sandbox -n kube-go get po -o json | jq '[.items[] | (.meta
 
 # kubectl get pod -o json |jq '.items[]|[.metadata.namespace+"/"+.metadata.name,[.status.containerStatuses[]|[.name,.state]]]'
 # kube sandbox -n wantedly get po -o json | jq '.items[] | (.metadata.namespace + "/" + .metadata.name) as $slug | .status.containerStatuses[] | { slug: $slug, container: .name, state: .state }|select(.state|has("terminated"))'
-curl -X POST -H "Content-type: application/json" -d "{$k8s_state_data}" 'https://app.datadoghq.com/api/v1/series?api_key=$API_KEY'
+curl -X POST -H "Content-type: application/json" -d "$k8s_state_data" "https://api.datadoghq.com/api/v1/series?api_key=$API_KEY"
+
